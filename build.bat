90251015cca5cb6f79290e990844281a9bd7cc4e
js <skylinertwo@aol.com>
2025-03-06 13:17:44 +0100

    add basic init and build scripts

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+
+RMDIR dist /S /Q
+
+cmake --build --preset vs2022-windows --config Release
+if %ERRORLEVEL% NEQ 0 exit 1
+
+xcopy "build\release\*.dll" "dist\SKSE\Plugins\" /I /Y
+xcopy "build\release\*.pdb" "dist\SKSE\Plugins\" /I /Y
+
+xcopy "package" "dist" /I /Y /E
---
+++ init.bat
@@ -0,0 +1,3 @@
+git init .
+git submodule update --init --recursive
+cmake --preset vs2022-windows
